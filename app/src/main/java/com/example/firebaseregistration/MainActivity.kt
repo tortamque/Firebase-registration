9f9bd2ba2f772774d8dad41736cd5aa433f05210
Vladyslav <90132962+tortamque@users.noreply.github.com>
2023-07-13 14:54:59 +0300

    Merge pull request #1
    * Enable view binding

--- app/src/main/java/com/example/firebaseregistration/MainActivity.kt
+++ app/src/main/java/com/example/firebaseregistration/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.firebaseregistration
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.firebaseregistration.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
