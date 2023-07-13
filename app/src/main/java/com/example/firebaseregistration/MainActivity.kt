8233e96bf57073d5f0eb5731a4a724d659a83ad2
Vladyslav <90132962+tortamque@users.noreply.github.com>
2023-07-13 17:20:59 +0300

    Merge pull request #3
    * Update gitignore
    
    * Add Firebase support
    
    * Create data class for Users
    
    * Create functionality for adding a user to Firebase

--- app/src/main/java/com/example/firebaseregistration/MainActivity.kt
+++ app/src/main/java/com/example/firebaseregistration/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.firebaseregistration
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.firebaseregistration.databinding.ActivityMainBinding
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
@@ -12,5 +15,10 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        binding.floatingActionButton.setOnClickListener {
+            val intent = Intent(this, AddUserActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
