e309cea8dba8f81ac07a7eabfbcded1296b11500
tortamque <90132962+tortamque@users.noreply.github.com>
2023-07-14 17:08:21 +0300

    Implement functionality to update user data

--- app/src/main/java/com/example/firebaseregistration/Activities/UpdateUserActivity.kt
+++ app/src/main/java/com/example/firebaseregistration/Activities/UpdateUserActivity.kt
@@ -2,10 +2,15 @@ package com.example.firebaseregistration.Activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.firebaseregistration.databinding.ActivityUpdateUserBinding
+import com.google.firebase.database.FirebaseDatabase
 
 class UpdateUserActivity : AppCompatActivity() {
     lateinit var binding: ActivityUpdateUserBinding
+
+    val database = FirebaseDatabase.getInstance()
+    val dbReference = database.reference.child("Users")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,6 +20,10 @@ class UpdateUserActivity : AppCompatActivity() {
         supportActionBar?.title = "Update User"
 
         initData()
+
+        binding.updateButton.setOnClickListener {
+            updateData()
+        }
     }
 
     fun initData(){
@@ -25,5 +34,26 @@ class UpdateUserActivity : AppCompatActivity() {
         binding.nameEditTextUpdate.setText(name)
         binding.ageEditTextUpdate.setText(age)
         binding.emailEditTextUpdate.setText(email)
+    }
+
+    fun updateData(){
+        val newName = binding.nameEditTextUpdate.text.toString()
+        val newAge = binding.ageEditTextUpdate.text.toString().toInt()
+        val newEmail = binding.emailEditTextUpdate.text.toString()
+        val id = intent.getStringExtra("id").toString()
+
+        val userMap = mutableMapOf<String, Any>(
+            "id" to id,
+            "name" to newName,
+            "age" to newAge,
+            "email" to newEmail
+        )
+
+        dbReference.child(id).updateChildren(userMap).addOnCompleteListener { task ->
+            if(task.isSuccessful){
+                Toast.makeText(applicationContext, "The user has been updated", Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
     }
 }
