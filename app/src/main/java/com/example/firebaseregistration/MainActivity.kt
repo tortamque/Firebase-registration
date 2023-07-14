6792b6b236b8f6817dc6748cdc5737980d8c4fca
tortamque <90132962+tortamque@users.noreply.github.com>
2023-07-14 14:22:05 +0300

    Add functionality to retrieve data from Firebase

--- app/src/main/java/com/example/firebaseregistration/MainActivity.kt
+++ app/src/main/java/com/example/firebaseregistration/MainActivity.kt
@@ -4,11 +4,16 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.firebaseregistration.databinding.ActivityMainBinding
-import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
+
+    val database = FirebaseDatabase.getInstance()
+    val dbReference = database.getReference().child("Users")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,5 +25,19 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, AddUserActivity::class.java)
             startActivity(intent)
         }
+
+        retrieveData()
+    }
+
+    fun retrieveData(){
+        dbReference.addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                for (dbUser in snapshot.children){
+                    val user = dbUser.getValue(Users::class.java)
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {}
+        })
     }
 }
